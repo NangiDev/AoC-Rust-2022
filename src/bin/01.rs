f6266349da235bee69e1511cd5f92fed1bc8be7b
Joel Setterberg <NangiDev@users.noreply.github.com>
2022-12-08 08:02:57 +0000

    Improved day 1

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -1,42 +1,30 @@
 pub fn part_one(input: &str) -> Option<u32> {
-    let calories: Vec<&str> = input.split('\n').collect();
-
-    let mut sum_cal: Vec<u32> = Vec::new();
-    let mut tmp_sum = 0;
-    
-    for c in &calories {
-        if c.is_empty() {
-            sum_cal.push(tmp_sum);
-            tmp_sum = 0;
-            continue;
-        }
-
-        tmp_sum += c.parse::<u32>().unwrap();
-    }
-
-    Some(*sum_cal.iter().max().unwrap())
+    input
+        .split("\n\n")
+        .map(|elf_load| {
+            elf_load
+                .lines()
+                .filter_map(|item| item.parse::<u32>().ok())
+                .sum::<u32>()
+        })
+        .max()
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    let calories: Vec<&str> = input.split('\n').collect();
+    let mut result: Vec<u32> = input
+        .split("\n\n")
+        .map(|elf_load| {
+            elf_load
+                .lines()
+                .filter_map(|item| item.parse::<u32>().ok())
+                .sum::<u32>()
+        })
+        .collect();
 
-    let mut sum_cal: Vec<u32> = Vec::new();
-    let mut tmp_sum = 0;
-    
-    for c in &calories {
-        if c.is_empty() {
-            sum_cal.push(tmp_sum);
-            tmp_sum = 0;
-            continue;
-        }
+    result.sort();
+    result.reverse();
 
-        tmp_sum += c.parse::<u32>().unwrap();
-    }
-
-    sum_cal.sort();
-    sum_cal.reverse();
-
-    Some(sum_cal[..3].iter().sum::<u32>())
+    Some(result[..3].iter().sum())
 }
 
 fn main() {

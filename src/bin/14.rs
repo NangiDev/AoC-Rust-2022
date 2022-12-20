9fd95af2706ac401e8679be26440a1f8d3d02fb4
NangiDev <j.settrbrg@gmail.com>
2022-12-20 10:15:54 +0100

    Solved

--- src/bin/14.rs
+++ src/bin/14.rs
@@ -48,26 +48,109 @@ pub fn part_one(input: &str) -> Option<u32> {
     let start_point = (500, 0);
     let mut sand = start_point.clone();
 
-    let dir: [(i32, i32); 3] = [(0, 1), (-1, 1), (1, 1)];
+    let mut stone_count = 0;
     loop {
         if sand.1 >= max_y {
             break;
         }
 
-        for d in dir {
-            let n_s: (u32, u32) = ((sand.0 as i32 + d.0) as u32, (sand.1 as i32 + d.1) as u32);
-            if taken_nodes.contains(&n_s) {
-                taken_nodes.push(sand);
-                break;
+        if !taken_nodes.contains(&(sand.0, sand.1 + 1)) {
+            sand.1 += 1;
+            continue;
+        } else if !taken_nodes.contains(&(sand.0 - 1, sand.1 + 1)) {
+            sand.0 -= 1;
+            sand.1 += 1;
+            continue;
+        } else if !taken_nodes.contains(&(sand.0 + 1, sand.1 + 1)) {
+            sand.0 += 1;
+            sand.1 += 1;
+            continue;
+        } else {
+            stone_count += 1;
+            taken_nodes.push(sand);
+            sand = start_point.clone();
+        }
+    }
+
+    Some(stone_count)
+}
+
+pub fn part_two(input: &str) -> Option<u32> {
+    let mut max_y = 0;
+    let mut taken_nodes: Vec<(u32, u32)> = Vec::new();
+    for line in input.lines() {
+        let coord = line.split(" -> ").collect::<Vec<&str>>();
+        let coord = coord
+            .iter()
+            .map(|c| {
+                c.split(',')
+                    .map(|c| c.parse::<u32>().unwrap())
+                    .collect::<Vec<u32>>()
+            })
+            .collect::<Vec<Vec<u32>>>();
+        let mut coord = coord
+            .windows(2)
+            .map(|win| Node::Line((win[0][0], win[0][1]), (win[1][0], win[1][1])))
+            .collect::<Vec<Node>>();
+
+        for Node::Line(start, end) in &mut coord {
+            if start.1 > max_y {
+                max_y = start.1;
+            }
+            taken_nodes.push(*start);
+            while start != end {
+                if start.0 == end.0 {
+                    if start.1 < end.1 {
+                        start.1 += 1;
+                    } else {
+                        start.1 -= 1;
+                    }
+                } else if start.1 == end.1 {
+                    if start.0 < end.0 {
+                        start.0 += 1;
+                    } else {
+                        start.0 -= 1;
+                    }
+                }
+                taken_nodes.push(*start);
             }
         }
     }
 
-    None
-}
+    let start_point = (500, 0);
+    let mut sand = start_point.clone();
 
-pub fn part_two(input: &str) -> Option<u32> {
-    None
+    let mut stone_count = 0;
+    loop {
+        if sand.1 == max_y + 1 {
+            stone_count += 1;
+            taken_nodes.push(sand);
+            sand = start_point.clone();
+            continue;
+        }
+
+        if !taken_nodes.contains(&(sand.0, sand.1 + 1)) {
+            sand.1 += 1;
+            continue;
+        } else if !taken_nodes.contains(&(sand.0 - 1, sand.1 + 1)) {
+            sand.0 -= 1;
+            sand.1 += 1;
+            continue;
+        } else if !taken_nodes.contains(&(sand.0 + 1, sand.1 + 1)) {
+            sand.0 += 1;
+            sand.1 += 1;
+            continue;
+        } else {
+            stone_count += 1;
+            taken_nodes.push(sand);
+            if sand == start_point {
+                break;
+            }
+            sand = start_point.clone();
+        }
+    }
+
+    Some(stone_count)
 }
 
 fn main() {
@@ -83,12 +166,12 @@ mod tests {
     #[test]
     fn test_part_one() {
         let input = advent_of_code::read_file("examples", 14);
-        assert_eq!(part_one(&input), None);
+        assert_eq!(part_one(&input), Some(24));
     }
 
     #[test]
     fn test_part_two() {
         let input = advent_of_code::read_file("examples", 14);
-        assert_eq!(part_two(&input), None);
+        assert_eq!(part_two(&input), Some(93));
     }
 }
